5305f80d335e287ff889aa4f96faa3f2d513af58
Mauro Moledo <mauromoledo@Mauros-MacBook-Pro.local>
2022-01-02 03:16:28 -0300

    Updates

---
+++ component---src-pages-404-js-2aa6645cca1e17210048.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[883],{9616:function(e,t,n){n.r(t);var u=n(7294);t.default=function(){return u.createElement(u.Fragment,null,u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2aa6645cca1e17210048.js.map
